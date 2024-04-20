58a1d4f71b50280462f6e74b1fbb2f586b8773c9
gregorgii <mavringosha322@gmail.com>
2024-04-21 00:23:45 +0300

    done krest

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <SFML/Graphics.hpp>
-
+#include <cmath>
+using namespace sf;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+    RectangleShape line1(Vector2f(150, 3));
+    line1.setPosition(-75.f/sqrt(2) + 200, -75.f/sqrt(2) + 200);
+    line1.rotate(45);
+    RectangleShape line2(Vector2f(150, 3));
+    line2.setPosition( -75.f/sqrt(2) + 200,  75.f/sqrt(2) + 200);
+    line2.rotate(-45);
 
     while (window.isOpen())
     {
-        sf::Event event;
+        sf::Event event{};
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -16,7 +21,8 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(line1);
+        window.draw(line2);
         window.display();
     }
 
